c63c9dbaae66fe22494667112b15fc1ff65bccde
Santhosh Sundar <ssundar3@sapient.com>
2017-12-02 13:40:36 +0530

    added inline comments

--- reducers/cart.js
+++ reducers/cart.js
@@ -1,12 +1,23 @@
   export default function reducer(state = [], action) {
+
+  // Our goal here is to perform actions on cart items without mutating the State
+  // Mutating the State can lead to performance issues as it could unnecessarily 
+  // re-render the component.
+
   switch (action.type) {
     case "ADD_TO_CART": {
+
+      // This adds new item to the cart array, in this the State without mutating it.
       return [
         ...state, action.payload
       ]
       break;
     }
     case "UPDATE_CART": {
+
+      // This finds the item that is already available in the cart array and updates 
+      // the item's quantity without mutating the State
+
       return state.map((item, index) => {
           if(index !== action.index) {
               return item;
@@ -18,13 +29,12 @@
           ]    
       });
     }
-    case "REMOVE_ITEM": {      
+    case "REMOVE_ITEM": { 
+      
+      // Removes the item from cart array without mutating the state.
+      // The Array.prototype.filter() method prevents us from mutating the array
+
       const itemIndex = state.findIndex(i => i.item === action.payload);
-
-      // let newArray = state.slice();
-      // newArray.splice(index, 1);
-      // return newArray;
-
       return state.filter((item, index) => index != itemIndex);
     }
   }
